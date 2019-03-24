94e101559f18fe3236a1113babd60a1f7a78171c
Klensch Lucas <klensch.lucas@gmail.com>
2019-03-24 23:39:54 +0200

    Final commit

--- Level 3/auth/app.js
+++ Level 3/auth/app.js
@@ -1,68 +1,117 @@
+/**
+ * Module dependencies.
+ */
 const express = require('express');
-const path = require('path');
-const favicon = require('static-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const logger = require('morgan');
+const chalk = require('chalk');
+const errorHandler = require('errorhandler');
+const MongoStore = require('connect-mongo')(session);
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const expressValidator = require('express-validator');
 
-const routes = require('./routes/index');
-const users = require('./routes/users');
-const db = require('./db');
+/**
+ * Controllers (route handlers).
+ */
+const controller = require('./routes/index');
 
+/**
+ * API keys and Passport configuration.
+ */
+const passportConfig = require('./config/passport');
+
+/**
+ * Create Express server.
+ */
 const app = express();
 
-// view engine setup
+/**
+ * Connect to MongoDB.
+ */
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.connect('mongodb://zaio-writer:qwerty123@ds139775.mlab.com:39775/zaio-profile');
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+    console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+    process.exit();
+});
+
+/**
+ * Express configuration.
+ */
+app.set('host', process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+// app.use(expressValidator());
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: 'This-is-my-secret',
+    cookie: {maxAge: 1209600000}, // two weeks in milliseconds
+    store: new MongoStore({
+        url: 'mongodb://zaio-writer:qwerty123@ds139775.mlab.com:39775/zaio-profile',
+        autoReconnect: true,
+    })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forwarding to error handler
-app.use(function (req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-//// Connect to the db
-db.connect('mongodb://zaio-writer:qwerty123@ds139775.mlab.com:39775/zaio-profile', function (err) {
-    if (err) {
-        console.log('Unable to connect to Mongo.');
-        process.exit(1)
-    } else {
-        console.log('Mongo is up and running.');
+app.use((req, res, next) => {
+    // After successful login, redirect back to the intended page
+    if (!req.user
+        && req.path !== '/login'
+        && req.path !== '/signup'
+        && !req.path.match(/^\/auth/)
+        && !req.path.match(/\./)) {
+        req.session.returnTo = req.originalUrl;
+    } else if (req.user
+        && (req.path === '/profile' || req.path.match(/^\/api/))) {
+        req.session.returnTo = req.originalUrl;
     }
+    next();
 });
-/// error handlers
+app.use('/', express.static(path.join(__dirname, 'public'), {maxAge: 31557600000}));
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+/**
+ * Primary app routes.
+ */
+app.get('/', controller.getLogin);
+app.post('/', controller.postLogin);
+app.get('/logout', controller.logout);
+app.get('/signup', controller.getSignup);
+app.post('/signup', controller.postSignup);
+app.get('/profile', passportConfig.isAuthenticated, controller.getAccount);
+app.get('/edit-profile', passportConfig.isAuthenticated, controller.getAccountProfile);
+app.post('/profile', passportConfig.isAuthenticated, controller.postUpdateProfile);
+
+/**
+ * Error Handler.
+ */
+if (process.env.NODE_ENV === 'development') {
+    // only use in development
+    app.use(errorHandler());
+} else {
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.status(500).send('Server Error');
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+    console.log('  Press CTRL-C to stop\n');
 });
-
 
 module.exports = app;
--- Level 3/auth/routes/index.js
+++ Level 3/auth/routes/index.js
@@ -1,62 +1,145 @@
-const express = require('express');
-const router = express.Router();
-const crypto = require('crypto');
-var db = require('../db');
+const User = require('../models/User');
+const passport = require('passport');
+/**
+ * GET /login
+ * Login page.
+ */
+exports.getLogin = (req, res) => {
+    res.render('signin', {
+        title: 'Login'
+    });
+};
 
+/**
+ * POST /login
+ * Sign in using email and password.
+ */
+exports.postLogin = (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect('/');
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            res.redirect(req.session.returnTo || '/');
+        });
+    })(req, res, next);
+};
 
-/* GET Signin page. */
-router.get('/', function (req, res) {
-    res.render('signin', {title: 'Sign', error: '', message: ''});
-});
+/**
+ * GET /logout
+ * Log out.
+ */
+exports.logout = (req, res) => {
+    req.logout();
+    req.session.destroy((err) => {
+        if (err) console.log('Error : Failed to destroy the session during logout.', err);
+        req.user = null;
+        res.redirect('/');
+    });
+};
 
-/* Signin */
-router.post('/', function (req, res) {
-    let error = 'Email or Password not correct';
-    let email = req.body.email;
-    let hash = crypto.createHash('sha256').update(req.body.password).digest('hex');
-    console.log(hash);
-    res.render('signin', {title: 'Signin', error: error, message: ''});
-    res.render('profile', {title: 'Signin'});
-});
+/**
+ * GET /signup
+ * Signup page.
+ */
+exports.getSignup = (req, res) => {
+    if (req.user) {
+        return res.redirect('/profile');
+    }
+    res.render('signup', {
+        title: 'Create Account'
+    });
+};
 
-/* GET Signup page */
-router.get('/signup', function (req, res) {
-    res.render('signup', {title: 'Signin', message: 'You can now login'});
-});
+/**
+ * POST /signup
+ * Create a new local account.
+ */
+exports.postSignup = (req, res, next) => {
 
-/* Signup */
-router.post('/signup', function (req, res) {
-    console.log(req.body.name);
-    res.render('signup', {title: 'Signin', message: 'You can now login'});
-});
+    if (req.body.password !== req.body.confirmpassword) {
+        res.redirect('/signup');
+    }
 
-router.get('/profile', function (req, res) {
-    let user = {
-        name: 'Klensch',
-        surname: 'Lucas',
-        email: 'klensch.lucas@gmail.com',
-        age: 21
-    };
-    res.render('profile', {title: 'Profile', error: '', message: '', user: user});
-});
-router.get('/edit-profile', function (req, res) {
-    let user = {
-        name: 'Klensch',
-        surname: 'Lucas',
-        email: 'klensch.lucas@gmail.com',
-        age: 21
-    };
-    res.render('edit-profile', {title: 'Edit Profile', message: '', user: user});
-});
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name,
+        surname: req.body.surname,
+        age: req.body.age,
+        degree: req.body.degree,
+        fav: req.body.fav
+    });
+    User.findOne({email: req.body.email}, (err, existingUser) => {
+        if (err) {
+            return next(err);
+        }
+        if (existingUser) {
+            return res.redirect('/');
+        }
+        user.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            req.logIn(user, (err) => {
+                if (err) {
+                    return next(err);
+                }
+                res.redirect('/');
+            });
+        });
+    });
+};
 
-router.post('/profile', function (req, res) {
-    let user = {
-        name: 'Klensch',
-        surname: 'Lucas',
-        email: 'klensch.lucas@gmail.com',
-        age: 21
-    };
-    res.render('profile', {title: 'Edit Profile', user: user, error: '', message: ''});
-});
+/**
+ * GET /profile
+ * Profile page.
+ */
+exports.getAccount = (req, res) => {
+    res.render('profile', {
+        title: 'Profile',
+        user: req.user
+    });
+};
+/**
+ * GET /edit-profile
+ * Profile page.
+ */
+exports.getAccountProfile = (req, res) => {
+    res.render('edit-profile', {
+        title: 'Edit Profile',
+        user: req.user
+    });
+};
 
-module.exports = router;
+/**
+ * POST /profile
+ * Update profile information.
+ */
+exports.postUpdateProfile = (req, res, next) => {
+    User.findById(req.user.id, (err, user) => {
+        if (err) {
+            return next(err);
+        }
+        user.name = req.body.name;
+        user.surname = req.body.surname;
+        user.age = req.body.age;
+        user.degree = req.body.degree;
+        user.fav = req.body.fav;
+        user.save((err) => {
+            if (err) {
+                if (err.code === 11000) {
+                    return res.redirect('/profile');
+                }
+                return next(err);
+            }
+            res.redirect('/profile');
+        });
+    });
+};
